a9c7f2f08ff155490251077cd0e4bf7e20956951
godonypeter <96230135+godonypeter@users.noreply.github.com>
2026-04-29 22:28:51 +0200

    Delete forraskod directory

--- forraskod/ellenorzes.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(substr($_SERVER['PHP_SELF'], -9)!='index.php'){
-    echo("<div style='background-color:red; color:white; padding:25px;'><h2>Védett oldal</h2><p>Ezt az oldalt nincs jogosultságod direktben elérni!</p><p>A próbálkozást naplóztuk</p></div>");
-    die;
-  }
-?>
